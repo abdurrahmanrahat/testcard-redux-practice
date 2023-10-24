88539a7b9b4729e9e6368e44a1b7d8a7659b1f8a
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-24 18:04:44 +0600

    login page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,6 +6,7 @@ import Settings from "../pages/Settings";
 import Profile from "../pages/Profile";
 import PrivateRoute from "../components/layouts/PrivateRoute";
 import Archive from "../pages/Archive";
+import Login from "../pages/Login";
 const routes = createBrowserRouter([
   {
     path: "/",
@@ -36,6 +37,14 @@ const routes = createBrowserRouter([
         element: <Profile />,
       },
     ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
   },
 ]);
 
