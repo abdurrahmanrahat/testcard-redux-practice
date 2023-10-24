6d39cf95425a8ee14c44967444e74afbbe4f977d
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-10-24 16:32:09 +0600

    modal details function

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,16 +2,25 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateStatus, userTasks } from "../../redux/features/tasks/tasksSlice";
+import TaskDetailsModal from "./TaskDetailsModal";
 
 const MyTasks = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [taskId, setTaskId] = useState(0);
+
   const { tasks, userSpecificTasks } = useSelector((state) => state.tasksSlice);
   const { name: userName } = useSelector((state) => state.userSlice);
   // console.log(name);
-
   const dispatch = useDispatch();
+
+  // handle modal function
+  const handleModal = (id) => {
+    setTaskId(id);
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     dispatch(userTasks(userName));
@@ -19,6 +28,12 @@ const MyTasks = () => {
 
   return (
     <div>
+      <TaskDetailsModal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        id={taskId}
+      ></TaskDetailsModal>
+
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {userSpecificTasks.map((item) => (
@@ -28,7 +43,11 @@ const MyTasks = () => {
           >
             <h1>{item.title}</h1>
             <div className="flex gap-3">
-              <button className="grid place-content-center" title="Details">
+              <button
+                onClick={() => handleModal(item.id)}
+                className="grid place-content-center"
+                title="Details"
+              >
                 <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
               </button>
               <button
--- src/components/tasks/TaskDetailsModal.jsx
+++ src/components/tasks/TaskDetailsModal.jsx
@@ -1,10 +1,17 @@
+import { useSelector } from "react-redux";
+import Modal from "../ui/Modal";
 
-const TaskDetailsModal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const TaskDetailsModal = ({ isOpen, setIsOpen, id }) => {
+  const { tasks } = useSelector((state) => state.tasksSlice);
+
+  const task = tasks.find((task) => task.id === id);
+//   console.log(task, tasks, id);
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={task?.title}>
+      <p>{task?.description}</p>
+    </Modal>
+  );
 };
 
 export default TaskDetailsModal;
